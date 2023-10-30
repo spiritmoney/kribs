221803485e0f9d0860961ffc43427a38197740b9
King Odim <kingsleyodim@gmail.com>
2023-10-30 15:35:54 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-red-600 text-4xl text-center hover:text-orange-500">Kribs</h1>
+    </>
+  )
+}
+
+export default App
